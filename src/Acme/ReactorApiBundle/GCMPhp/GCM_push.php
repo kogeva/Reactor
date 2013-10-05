71530f72b7d82e03759d82243b0fbac81dc30c63
moskalyov <moskalyovd@gmail.com>
2013-10-05 12:26:29 +0300

    add android push notification

---
+++ src/Acme/ReactorApiBundle/GCMPhp/GCM_push.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "sendNotification.php";
+
+$serializeData = $argv[1];
+$data = unserialize($serializeData);
+
+foreach($data as $entity)
+{
+
+    $token = $entity[0];
+    $text = $entity[1];
+    $notRead = $entity[2];
+    $messageId = $entity[3];
+    $photo = $entity[4];
+    $reactionPhoto = $entity[5];
+    $messageText = $entity[6];
+    $userId = $entity[7];
+
+    $browserApiKey = 'AIzaSyDcIOqygik-KewTHpm0dDRDcDlAYaZxEzs';
+
+    $response = sendNotification(
+        $browserApiKey,
+        $token,
+        array(
+            'badge' => $notRead,
+            'text' => $text,
+            'message_id' => $messageId,
+            'photoLink' => $photo,
+            'reactorPhotoLink' => $reactionPhoto,
+            'message_text' => $messageText,
+            'user_id' => $userId
+        )
+    );
+
+    echo $response;
+}
