bbe57048f43a4950aad82246901cd030847a1650
moskalyov <moskalyovd@gmail.com>
2013-10-07 15:17:55 +0300

    fix gcm

--- src/Acme/ReactorApiBundle/GCMPhp/GCM_push.php
+++ src/Acme/ReactorApiBundle/GCMPhp/GCM_push.php
@@ -1,61 +1,69 @@
 <?php
 
-$serializeData = $argv[1];
-
-$data = unserialize($serializeData);
-
-$apiKey = "AIzaSyA93BAblwJIDa3bEknH6dUMH4OjrjWCwzU";
-$url = 'https://android.googleapis.com/gcm/send';
-
-
-foreach($data as $entity)
+function sendNotification($data)
 {
+    /*$serializeData = $argv[1];
 
-    $token         = $entity[0];
-    $text          = $entity[1];
-    $notRead       = $entity[2];
-    $messageId     = $entity[3];
-    $photo         = $entity[4];
-    $reactionPhoto = $entity[5];
-    $messageText   = $entity[6];
-    $userId        = $entity[7];
+$data = unserialize($serializeData);*/
 
-    $registrationIDs = array($token);
+    $apiKey = "AIzaSyA93BAblwJIDa3bEknH6dUMH4OjrjWCwzU";
+    $url = 'https://android.googleapis.com/gcm/send';
 
-    $pushNotification = array(
-        'badge'               => $notRead,
-        'text'                => $text,
-        'message_id'          => $messageId,
-        'linkToPhoto'         => $photo,
-        'linkToReactionPhoto' => $reactionPhoto,
-        'messageText'         => $messageText,
-        'userId'              => $userId
-    );
+    date_default_timezone_set('Europe/Rome');
 
-    $fields = array(
-        'registration_ids' => $registrationIDs,
-        'data' => $pushNotification,
-    );
-
-    $headers = array(
-        'Authorization: key=' . $apiKey,
-        'Content-Type: application/json'
-    );
-
-    $ch = curl_init();
-
-    curl_setopt( $ch, CURLOPT_URL, $url );
-    curl_setopt( $ch, CURLOPT_POST, true );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    error_reporting(-1);
 
 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    foreach($data as $entity)
+    {
 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $fields ));
+        $token         = $entity[0];
+        $text          = $entity[1];
+        $notRead       = $entity[2];
+        $messageId     = $entity[3];
+        $photo         = $entity[4];
+        $reactionPhoto = $entity[5];
+        $messageText   = $entity[6];
+        $userId        = $entity[7];
 
-    $result = curl_exec($ch);
+        $registrationIDs = array($token);
 
-    curl_close($ch);
-    echo $result;
+        $pushNotification = array(
+            'badge'               => $notRead,
+            'text'                => $text,
+            'message_id'          => $messageId,
+            'linkToPhoto'         => $photo,
+            'linkToReactionPhoto' => $reactionPhoto,
+            'messageText'         => $messageText,
+            'userId'              => $userId
+        );
+
+        $fields = array(
+            'registration_ids' => $registrationIDs,
+            'data' => $pushNotification,
+        );
+
+        $headers = array(
+            'Authorization: key=' . $apiKey,
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_POST, true );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $fields ));
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+        echo $result;
+    }
 }
+
