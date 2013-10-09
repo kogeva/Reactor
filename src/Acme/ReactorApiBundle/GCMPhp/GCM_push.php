d272b3f98426d2045139aa793777e7b0f02061d6
moskalyov <moskalyovd@gmail.com>
2013-10-09 19:16:39 +0300

    android gcm

--- src/Acme/ReactorApiBundle/GCMPhp/GCM_push.php
+++ src/Acme/ReactorApiBundle/GCMPhp/GCM_push.php
@@ -12,7 +12,6 @@
     $ch = curl_init();
     curl_setopt( $ch, CURLOPT_URL, $url );
     curl_setopt( $ch, CURLOPT_POST, true );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 
 foreach($data as $entity)
